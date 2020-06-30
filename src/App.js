72672e7a7cee695259d832c4b236fb43356b348b
Rizwan Shivalli <rizwan.a@simplificommerce.com>
2020-06-30 12:49:41 +0530

    navigation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import About from "./screens/About";
+import Profile from "./screens/Profile";
+import ErrorPage from "./screens/ErrorPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+
+            <Route path="/about" component={About} />
+
+            <Route path="/profile" component={Profile} />
+            <Route component={ErrorPage} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/screens/ErrorPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return <h1>An Error has occured</h1>;
+};
+
+export default ErrorPage;
